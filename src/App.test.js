df13550ab37adf91b427f35d13f0512679d2f54f
Hoslack <hoslack.ochieng@andela.com>
2019-08-16 21:24:35 +0300

    Add more tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+// test('Test App exists', ()=> {
+//   const app = shallow(<App/>);
+// });
+// test('null', () => {
+//   const n = null;
+//   expect(n).toBeNull();
+//   expect(n).toBeDefined();
+//   expect(n).not.toBeUndefined();
+//   expect(n).not.toBeTruthy();
+//   expect(n).toBeFalsy();
+// });
+test('App exists', () => {
+  const app = renderer.create(
+    <App/>,
+  );
+  expect(app).toBeDefined();
+  expect(app).toMatchSnapshot();
 });
